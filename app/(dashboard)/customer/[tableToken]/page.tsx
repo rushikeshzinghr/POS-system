1bece4dbad545661dfebb087f4c5ebb934c6bc87
rushikesh-bhor-zinghr <rushikesh.zinghr@gmail.com>
2026-06-11 16:01:17 +0530

    test

--- app/(dashboard)/customer/[tableToken]/page.tsx
+++ app/(dashboard)/customer/[tableToken]/page.tsx
@@ -1,13 +1,13 @@
 import { redirect } from "next/navigation";
 
 type Props = {
-  params: {
+  params: Promise<{
     tableToken: string;
-  };
+  }>;
 };
 
-export default function TableTokenRedirect({ params }: Props) {
-  const { tableToken } = params;
+export default async function Page({ params }: Props) {
+  const { tableToken } = await params;
 
   // Redirect to the main customer page and pass the token as a query param
   redirect(`/customer?tableToken=${encodeURIComponent(tableToken)}`);
